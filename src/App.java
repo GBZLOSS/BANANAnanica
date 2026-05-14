ab191f44647e7062fb69fe9ecb63175af1f3c39d
HeitorrFv <heitorfv05@gmail.com>
2026-05-14 17:23:08 -0300

    a
    a

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Quem está lendo é extremamente viado. ou seja Tadashi e Heitor Fukushima e Nagazaki");
         System.out.println("ado ado ado o gabriel é viaaaaaaaaa....");
+        System.out.println("a");
+        System.out.println("b");
     }
 }
