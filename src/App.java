f6597ec8582aea98e672d9c2cb7f544f8f1d7e99
GBZLOSS <gabrielvearaujo@gmail.com>
2026-05-14 17:04:29 -0300

    TESTE ALTERAÇÃO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Quem está lendo é extremamente viado.");
+        System.out.println("Quem está lendo é extremamente viado. ou seja Tadashi e Heitor Fukushima e Nagazaki");
     }
 }
